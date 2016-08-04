4c06322018945a4df86156f3c84b2f8688f4b719
Pham Duc Minh <phamducminh1990@gmail.com>
2016-08-04 08:56:56 +0700

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,27 +5,26 @@ class HomeController < ApplicationController
   end
 
   def about
+    puts "#######################################"
+    conn=PGconn.connect(:hostaddr => "127.0.0.1", :port => 5432, :dbname => "ruby-example", :user => "postgres", :password => 'Hello12#')
+    res = conn.exec("SELECT * FROM user_models")
+    fieldArray = res.fields()
+    #
+    connection = ActiveRecord::Base.connection
+    res2 = connection.execute("SELECT * FROM user_models")
+    fieldArray2 = res2.fields()
+    #
+    puts "-----fieldArray1-----"
+    puts fieldArray
+    puts "-----fieldArray2-----"
+    puts fieldArray2
   end
 
   def show
     # @model = Message.find(params[:id])
     # @all_model = Message.all
     #
-    # conn=PGconn.connect( :hostaddr=>"127.0.0.1", :port=>5432, :dbname=>"ruby-example", :user=>"postgres", :password=>'Hello12#')
-    # res = conn.exec("SELECT * FROM messages")
-    # fieldArray = res.fields()
-    #
-    # connection = ActiveRecord::Base.connection
-    # res2 = connection.execute("SELECT * FROM messages")
-    # fieldArray2 = res2.fields()
-    #
-    # puts"fieldArray1"
-    #
-    # puts fieldArray
-    #
-    # puts"fieldArray2"
-    #
-    # puts fieldArray2
+
     #
     #
     # @temp = UserDetail.all
